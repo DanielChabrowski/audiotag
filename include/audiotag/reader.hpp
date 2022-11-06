5e2364308abb8d1ed071e25620eac16d78f97444
Daniel Chabrowski <danchabrowski@gmail.com>
2022-11-06 19:58:19 +0100

    Add nodiscard to reader interface

--- include/audiotag/reader.hpp
+++ include/audiotag/reader.hpp
@@ -10,11 +10,11 @@ class Reader
 public:
     virtual ~Reader() = default;
 
-    virtual std::size_t length() const = 0;
-    virtual std::size_t buffer_size() const = 0;
+    [[nodiscard]] virtual std::size_t length() const = 0;
+    [[nodiscard]] virtual std::size_t buffer_size() const = 0;
 
-    virtual std::size_t read(std::span<std::byte> buffer) = 0;
+    [[nodiscard]] virtual std::size_t read(std::span<std::byte> buffer) = 0;
 
-    virtual bool seek(long offset) = 0;
+    [[nodiscard]] virtual bool seek(long offset) = 0;
 };
 } // namespace audiotag
